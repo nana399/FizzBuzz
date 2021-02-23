b49d17277b8b3e907d79ddac506380f22a61af13
nana399 <kinakorabbit1182@gmail.com>
2021-02-24 05:38:43 +0900

    FizzBuzz課題

---
+++ FizzBuzz.rb
@@ -0,0 +1,15 @@
+i = 0
+while i < 30 do
+  i += 1
+
+  if i % 15 == 0
+    puts "FizzBuzz"
+  elsif i % 3 == 0
+    puts "Fizz"
+  elsif i % 5 == 0
+    puts "Buzz"
+  else
+    puts i.to_s
+  end
+end
+
